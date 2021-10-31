95062f7967414682e5c081bc63b2e70e902e0ea7
vlad-komanchy <vkomanchy@gmail.com>
2021-10-31 19:51:14 +0300

    feat: add node-cron for sending notifications
    - remove unneeded packages

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import { TaskObject } from 'types';
+import * as cron from 'node-cron';
 import { getWorkingDaysCount, roundHours, sumAllHours } from './utils';
 
 require('dotenv/config');
@@ -10,40 +11,51 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: false });
 
-const goalHours = 168;
+const HARDCODED_CHAT_ID = Number(process.env.CHAT_ID);
 
-const date = new Date();
-const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
-const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+cron.schedule('0 17 * * 1-5', () => {
+  const goalHours = 168;
 
-const startDate = new Date(`${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`);
-const endDate = new Date(`${lastDayOfMonth.getMonth() + 1}/${lastDayOfMonth.getDate()}/${lastDayOfMonth.getFullYear()}`);
+  const date = new Date();
+  const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+  const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
-toggle.getTimeEntries(firstDayOfMonth, lastDayOfMonth, (error: any, allDaysData: TaskObject[]) => {
-  if (error) return;
+  const startDate = new Date(`${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`);
+  const endDate = new Date(`${lastDayOfMonth.getMonth() + 1}/${lastDayOfMonth.getDate()}/${lastDayOfMonth.getFullYear()}`);
 
-  const dayStart = new Date();
-  dayStart.setHours(0, 0, 0, 0);
-
-  const dayEnd = new Date();
-  dayEnd.setHours(23, 59, 59, 999);
-
-  toggle.getTimeEntries(dayStart, dayEnd, (error: any, todayData: TaskObject[]) => {
+  toggle.getTimeEntries(firstDayOfMonth, lastDayOfMonth, (error: any, allDaysData: TaskObject[]) => {
     if (error) return;
 
-    const remainingDays = getWorkingDaysCount(startDate, endDate);
+    const dayStart = new Date();
+    dayStart.setHours(0, 0, 0, 0);
 
-    const currentDayHours = roundHours(sumAllHours(todayData));
+    const dayEnd = new Date();
+    dayEnd.setHours(23, 59, 59, 999);
 
-    const currentHoursSum = sumAllHours(allDaysData);
+    toggle.getTimeEntries(dayStart, dayEnd, (error: any, todayData: TaskObject[]) => {
+      if (error) return;
 
-    const remainingHours = roundHours(goalHours - currentHoursSum);
+      const remainingDays = getWorkingDaysCount(startDate, endDate);
 
-    const averageHoursPerDay = roundHours(remainingHours / remainingDays);
+      const currentDayHours = roundHours(sumAllHours(todayData));
 
-    bot.sendMessage(Number(process.env.CHAT_ID), `
+      const currentHoursSum = sumAllHours(allDaysData);
+
+      const remainingHours = roundHours(goalHours - currentHoursSum);
+
+      const averageHoursPerDay = roundHours(remainingHours / remainingDays);
+
+      if (!HARDCODED_CHAT_ID || HARDCODED_CHAT_ID === 0) {
+        console.error('Wrong chat id was provided');
+      }
+
+      bot.sendMessage(
+        HARDCODED_CHAT_ID,
+        `
       Спасибо за продуктивный рабочий день. Вы отработали ${currentDayHours} час(а/ов).
       До цели (${goalHours} часов) осталось - ${remainingHours} час(а/ов). Примерно ${averageHoursPerDay} в день.
-    `);
+      `,
+      );
+    });
   });
 });
